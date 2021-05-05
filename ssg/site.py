c8e10aad5bfa1043b3e980f3c9d2c02bb6f071ab
jtrickard <45494119+jtrickard@users.noreply.github.com>
2021-05-05 15:06:10 -0400

    updated parsers

--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 class Site:
@@ -20,7 +21,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print('Not Implemented')
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
     
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -30,3 +31,6 @@ class Site:
             elif path.is_file():
                 self.run_parser(path)
     
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
