b451b5367fffd455a14a92d9a07704381d6cf697
jtrickard <45494119+jtrickard@users.noreply.github.com>
2021-05-05 11:45:24 -0400

    created parser class and updated site class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,23 +1,30 @@
-from os import read
 from typing import List
 from pathlib import Path
+import shutil
 
 class Parser:
-    extensions = List[str]
+    extensions: List[str] = []
 
     def valid_extension(self,extension):
-        if extension in self.extensions:
-            return True
-        else:
-            return False
+        return extension in self.extensions
     
-    def parse(path, source, dest):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
-    def read(path):
-        with open(path) as file:
-            return read(file)
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
     
-    def write(path, dest, content, ext='html'):
-        full_path = self.dest / path.with_suffix(ext)
+    def write(self, path, dest, content, ext='.html'):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path,"w") as file:
+            file.write(content)
+    
+    def copy(self, path, source, dest):
+        self.copy2(path, dest / path.relative_to(source))
 
+class ResourceParser(Parser):
+    extensions = ['.jpg', '.png', '.gif', '.css', '.html']
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
